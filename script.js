cff275781b23cae29337a4a2b36fa83cf16f1c98
Sumit Kumar <Sumitkumarp8292@gmail.com>
2025-12-24 12:08:35 +0530

    message

--- script.js
+++ script.js
@@ -1,4 +1,57 @@
-/* IMAGE CAROUSEL */
+/* ========== QUIZ SECTION ========== */
+const quizData = [
+    {
+        question: "Which language is used for styling web pages?",
+        options: ["HTML", "CSS", "JavaScript"],
+        answer: "CSS"
+    },
+    {
+        question: "Which keyword is used to create a variable in JavaScript?",
+        options: ["int", "var", "float"],
+        answer: "var"
+    },
+    {
+        question: "Which method is used to fetch API data?",
+        options: ["fetch()", "get()", "pull()"],
+        answer: "fetch()"
+    }
+];
+
+let current = 0;
+let score = 0;
+
+function loadQuiz() {
+    document.getElementById("question").innerText =
+        quizData[current].question;
+
+    const optionsDiv = document.getElementById("options");
+    optionsDiv.innerHTML = "";
+
+    quizData[current].options.forEach(opt => {
+        let btn = document.createElement("button");
+        btn.innerText = opt;
+        btn.onclick = () => {
+            if (opt === quizData[current].answer) {
+                score++;
+            }
+        };
+        optionsDiv.appendChild(btn);
+    });
+}
+
+function nextQuestion() {
+    current++;
+    if (current < quizData.length) {
+        loadQuiz();
+    } else {
+        document.getElementById("score").innerText =
+            "Your Score: " + score + "/" + quizData.length;
+    }
+}
+
+loadQuiz();
+
+/* ========== IMAGE CAROUSEL ========== */
 let index = 1;
 
 function nextImage() {
@@ -14,7 +67,7 @@ function prevImage() {
         "https://picsum.photos/500/300?" + index;
 }
 
-/* FETCH API DATA */
+/* ========== API FETCH ========== */
 function getJoke() {
     fetch("https://official-joke-api.appspot.com/random_joke")
         .then(response => response.json())
@@ -22,7 +75,7 @@ function getJoke() {
             document.getElementById("joke").innerHTML =
                 data.setup + "<br><b>" + data.punchline + "</b>";
         })
-        .catch(error => {
+        .catch(() => {
             document.getElementById("joke").innerText =
                 "Error loading joke!";
         });
